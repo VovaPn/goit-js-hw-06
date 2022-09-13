babc4d3aed456070464d63e52a6e2a6fe2006ea7
Vovandir <106840463+Vovandir@users.noreply.github.com>
2022-09-13 20:40:23 +0300

    task 3 changes

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -10,22 +10,8 @@ const images = [
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
-  },
-];
+  },];
 
 const galleryUl=document.querySelector('.gallery');
 const imagesLi=images.map((image)=>`<li><img src="${image.url}" alt="${image.alt}"></li>`).join(' ')
 galleryUl.insertAdjacentHTML("afterbegin", imagesLi) 
-
-
-
-
-// const list = document.querySelector(".list");
-
-// const newTechnologies = ["React", "TypeScript", "Node.js"];
-// const markup = newTechnologies
-//   .map((technology) => `<li class="list-item new">${technology}</li>`)
-//   .join("");
-
-// list.insertAdjacentHTML("beforeend", markup);
-// list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
